c0204597a4e8f6b308eb73cb6507936cec6dbba5
Mahesh <54623929+maahi07m@users.noreply.github.com>
2020-03-20 14:21:26 +0530

    datatype

---
+++ data-type.java
@@ -0,0 +1,14 @@
+public class MyClass {
+  public static void main(string[] args){
+  int myInt = 9;
+  double myDouble = myInt;
+  float myFloatNum = 5.99f;
+  char myLetter = 'D';
+  boolean myText = "Hello";
+  System.out.println(myFloatNum);
+  System.out.println(myLetter);
+  System.out.println(myText);
+  System.out.println(myInt);
+  System.out.println(myDouble);
+  }
+ }
