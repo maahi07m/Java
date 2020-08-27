fb512b5395d07b0650ab9279466b5f47430894c0
Mahesh <54623929+maahi07m@users.noreply.github.com>
2020-08-27 19:09:11 +0530

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,5 @@
+//Filter method is used to get the defined output that is used in JS helps to optimise code
+const marks = [100,98,76,32,55,21,35];
+const passStudent = marks.filter( mark => mark>=35)
+console.log(passStudent)
+//[100, 98, 76, 55, 35]
